c2fcd762396f5a4fa849941f48ea255baa40a8bc
Dominic <75138982+dosc91@users.noreply.github.com>
2022-08-11 16:53:03 +0200

    Add files via upload

---
+++ R/colors_ndl.R
@@ -0,0 +1,8 @@
+#' Example Data Frame of Color Vectors created with NDL
+#'
+#' This data frame contains color vectors of 300 dimensions. It is mainly used for exercises.
+#'
+#' @usage data(colors_ndl)
+#'
+#' @source Schmitz, D., & Schneider, V. (2022). gdsm: General functions for Distributional SeMantics. R package version 0.1. URL: https://github.com/dosc91/gdsm
+"colors_ndl"
---
+++ R/kinship_ndl.R
@@ -0,0 +1,8 @@
+#' Example Data Frame of Kinship Vectors created with NDL
+#'
+#' This data frame contains kinship vectors of 300 dimensions. It is mainly used for exercises.
+#'
+#' @usage data(kinship_ndl)
+#'
+#' @source Schmitz, D., & Schneider, V. (2022). gdsm: General functions for Distributional SeMantics. R package version 0.1. URL: https://github.com/dosc91/gdsm
+"kinship_ndl"
