5ea9d45a890a9903256ca84cfc02087bf007d037
Dominic <75138982+dosc91@users.noreply.github.com>
2022-08-11 18:29:37 +0200

    Delete kinship.R

--- R/kinship.R
+++
@@ -1,8 +0,0 @@
-#' Example Data Frame of Kinship Vectors
-#'
-#' This data frame contains kinship vectors of 300 dimensions. It is mainly used for exercises.
-#'
-#' @usage data(kinship)
-#'
-#' @source Schmitz, D., & Schneider, V. (2022). gdsm: General functions for Distributional SeMantics. R package version 0.1. URL: https://github.com/dosc91/gdsm
-"kinship"
