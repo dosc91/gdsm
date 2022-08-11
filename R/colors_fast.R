3d232a7da4955839a9e8dbd7bdee57efe0fc9456
Dominic <75138982+dosc91@users.noreply.github.com>
2022-08-11 18:30:08 +0200

    Add files via upload

--- R/colors_fast.R
+++ R/colors_fast.R
@@ -1,8 +1,8 @@
-#' Example Data Frame of Color Vectors created with NDL
+#' Example Data Frame of Color Vectors created with fastText
 #'
 #' This data frame contains color vectors. It is mainly used for exercises.
 #'
-#' @usage data(colors_ndl)
+#' @usage data(colors_fast)
 #'
 #' @source Schmitz, D., & Schneider, V. (2022). gdsm: General functions for Distributional SeMantics. R package version 0.1. URL: https://github.com/dosc91/gdsm
-"colors_ndl"
+"colors_fast"
--- R/kinship_fast.R
+++ R/kinship_fast.R
@@ -1,8 +1,8 @@
-#' Example Data Frame of Kinship Vectors created with NDL
+#' Example Data Frame of Kinship Vectors created with fastText
 #'
 #' This data frame contains kinship vectors. It is mainly used for exercises.
 #'
-#' @usage data(kinship_ndl)
+#' @usage data(kinship_fast)
 #'
 #' @source Schmitz, D., & Schneider, V. (2022). gdsm: General functions for Distributional SeMantics. R package version 0.1. URL: https://github.com/dosc91/gdsm
-"kinship_ndl"
+"kinship_fast"
